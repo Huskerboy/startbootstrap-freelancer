ff0aba655d3a1905438a7902cf62e1bba8dfa0de
Huskerboy <jmeyer2534@gmail.com>
2017-04-13 00:00:03 -0500

    I added a python flask file and changed the website by adding Service, changing the contact me, and put in my bio,

---
+++ init.py
@@ -0,0 +1,11 @@
+import random
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def homepage():
+    return render_template("index.html")
+
+if __name__ == ("__main__"):
+    app.run(debug=True)
